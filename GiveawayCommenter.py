6c80afd87424ab45424aedd9ed68b930790f3be0
Landienzla <landienzla@gmail.com>
2021-04-11 22:05:28 +0300

    v1.0.1

--- GiveawayCommenter.py
+++ GiveawayCommenter.py
@@ -6,32 +6,37 @@ import pyautogui
 import time
 import itertools
 
-browser = webdriver.Chrome(ChromeDriverManager().install())
-browser.get("https://www.instagram.com")
+browser = webdriver.Chrome(ChromeDriverManager().install()) # install last version of chrome
+browser.get("https://www.instagram.com") #get instagram page at browser
+time.sleep(15) 
+username_area = browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input')
+username_area.click()
 time.sleep(15)
-browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input').click()
+username_area.send_keys("username")
+password_area = browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input')
+password_area.click()
 time.sleep(15)
-pyautogui.write("your-username")
-browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input').click()
-time.sleep(15)
-pyautogui.write("your-password")
+password_area.send_keys("password")
 browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]').click()
 time.sleep(15)
-browser.get("giveaway link")
+browser.get("the giveaway's link")
 time.sleep(15)
 
 userlist= [""]
 combineduserlist = list(itertools.combinations(userlist,2))
-a = 34
-for i in range(325):
-    browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/section[3]').click()
-    commentarea = browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/section[3]/div/form/textarea')
-    commentarea.send_keys(combineduserlist[a])
-    time.sleep(10)
-    browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/section[3]/div/form/button').click()
-    print(combineduserlist[a], a , ".Yorum")
-    a += 1
-    time.sleep(60)
+print(len(combineduserlist))
+
+for i in range(len(combineduserlist)):
+    try:
+        browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/section[3]').click()
+        commentarea = browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/section[3]/div/form/textarea')
+        commentarea.send_keys(combineduserlist[i])
+        time.sleep(10)
+        browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/section[3]/div/form/button[2]').click()
+        time.sleep(60)
+    except:
+        print("ERROR")
+        print("I was commenting" + combineduserlist[i])
 browser.stop_client()
 browser.close()
 
